0e9651e5adf7272ab956fd20618d8bc754872619
belifkutlu <elifkutlu41@gmail.com>
2022-07-22 08:32:09 +0300

    add GuardRoute component

---
+++ src/components/GuardRoute.tsx
@@ -0,0 +1,12 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useUser } from "../contexts/user";
+
+const GuardRoute = () => {
+  const userContext = useUser();
+  if (!userContext?.state.user) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Outlet />;
+};
+
+export default GuardRoute;
