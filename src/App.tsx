8445c480e80463c5c9eb32b332cc03b555652059
belifkutlu <elifkutlu41@gmail.com>
2022-07-22 08:39:25 +0300

    add styled component theme

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+
+import Login from "./pages/Login";
+import List from "./pages/List";
+import Account from "./pages/Account";
+import BottonBar from "./components/BottonBar";
+import Container from "./components/Container";
+import theme from "./theme";
+import { UserProvider } from "./contexts/user";
+import GuardRoute from "./components/GuardRoute";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <Container>
+          <BottomBarWrapper>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+
+              <Route element={<GuardRoute />}>
+                <Route path="/" element={<List />} />
+                <Route path="/account" element={<Account />} />
+              </Route>
+            </Routes>
+            <BottonBar />
+          </BottomBarWrapper>
+        </Container>
+      </UserProvider>
+    </ThemeProvider>
+  );
+}
+
+const BottomBarWrapper = styled.div`
+  height: 100vh;
+`;
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    black: "#121212",
+    gray: "#BBC3CF",
+    white: "#FFFFFF",
+    lightGray: "#FAFAFA;",
+    red: "#E82223",
+  },
+  sizes: {
+    containerWidth: "600px",
+  },
+};
+
+export default theme;
