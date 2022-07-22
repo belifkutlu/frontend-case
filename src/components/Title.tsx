1b696620e16378bbb2d4b2800071c78a84603aee
belifkutlu <elifkutlu41@gmail.com>
2022-07-22 08:34:42 +0300

    add title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+type Props = {
+  title: string;
+};
+
+export default function Title({ title }: Props) {
+  return <StyledTitle>{title}</StyledTitle>;
+}
+
+const StyledTitle = styled.h3`
+  color: ${({ theme }) => theme.colors.black};
+  font-weight: 600;
+  font-size: 32px;
+`;
