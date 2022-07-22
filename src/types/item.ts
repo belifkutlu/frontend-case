fe1c7d42bdc417d782f2f87de7f12768c6ef735c
belifkutlu <elifkutlu41@gmail.com>
2022-07-22 08:37:49 +0300

    add item type

---
+++ src/types/item.ts
@@ -0,0 +1,11 @@
+export type Item = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  rate: string;
+  distance: number;
+  price: number;
+  shippingCost: number;
+  tax: number;
+};
